d073dd87a643d31147ab840e1a9bf30dc923209b
nagai kenji <nagai-kj@nagai-galaxy.com>
2019-03-21 15:52:37 +0900

    [fix]入力チェックに引っかかった場合や、タグの入力を削除した場合、削除されない問題を解決

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,21 +3,28 @@ class Article < ApplicationRecord
   has_many :tags, through: :taggings
   accepts_nested_attributes_for :tags
 
+  has_many :searched_tags, ->(tags) { where(name: tags) }, through: :taggings, class_name: 'Tag'
+
   validates :title, presence: true
 
   MAX_TAGS = 4
 
   def tags_attributes=(tags)
+    tentative_tags = []
+
     tags.each do |_, attr|
       if (tag_name = attr[:name]).present?
         tag = Tag.find_or_create_by!(name: tag_name)
-        taggings.find_or_initialize_by(tag: tag)
+        tentative_tags << tag
       end
     end
+
+    # through: :taggingsしているので中間テーブルのtagginsも自動で作成される
+    self.tags.replace(tentative_tags)
   end
 
   def tags_build
-    (MAX_TAGS - tags.length).times do
+    (MAX_TAGS - taggings.length).times do
       tags.build
     end
   end
